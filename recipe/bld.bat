2c1e34b5983285f25ed4b54854942f5c579b5039
Andrii Osipov <aosipov@anaconda.com>
2026-02-05 08:54:27 -0800

    try conda-forge approach

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,76 +1,46 @@
 set LLVM_INSTALL_DIR=%LIBRARY_PREFIX%
 
-echo Building: shiboken6
-
-cd %SRC_DIR%\sources\shiboken6
-
-cmake -LAH -G "Ninja"                               ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"          ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"       ^
-    -DCMAKE_UNITY_BUILD=ON                          ^
-    -DCMAKE_UNITY_BUILD_BATCH_SIZE=32               ^
+:: build shiboken only
+cmake %CMAKE_ARGS% -LAH -G "Ninja"                               ^
+    -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
     -DPYTHON_SITE_PACKAGES="%SP_DIR:\=/%"           ^
-    -DCMAKE_BUILD_TYPE=Release                      ^
-    -DFORCE_LIMITED_API=OFF                         ^
     -DBUILD_TESTS=OFF                               ^
+    -DFORCE_LIMITED_API=OFF                         ^
     -DPython_EXECUTABLE="%PYTHON%"                  ^
-    .
+    -B build_shiboken %SRC_DIR:\=/%/sources/shiboken6
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+cmake --build build_shiboken --target install
 if errorlevel 1 exit 1
 
+:: write dummy shiboken metadata
 mkdir %SP_DIR%\shiboken6-%PKG_VERSION%.dist-info
 copy %RECIPE_DIR%\METADATA.shiboken6.in %SP_DIR%\shiboken6-%PKG_VERSION%.dist-info\METADATA
-copy %RECIPE_DIR%\INSTALLER.in %SP_DIR%\shiboken6-%PKG_VERSION%.dist-info\INSTALLER
 echo Version: %PKG_VERSION% >> %SP_DIR%\shiboken6-%PKG_VERSION%.dist-info\METADATA
+type %SP_DIR%\shiboken6-%PKG_VERSION%.dist-info\METADATA
 
-echo Done: shiboken6
-echo Building: pyside6
-
-cd %SRC_DIR%\sources\pyside6
-
-cmake -LAH -G "Ninja"                               ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"          ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"       ^
+:: build all
+cmake %CMAKE_ARGS% -LAH -G "Ninja"                               ^
     -DCMAKE_UNITY_BUILD=ON                          ^
     -DCMAKE_UNITY_BUILD_BATCH_SIZE=32               ^
     -DPYTHON_SITE_PACKAGES="%SP_DIR:\=/%"           ^
-    -DCMAKE_BUILD_TYPE=Release                      ^
-    -DPython_EXECUTABLE="%PYTHON%"                  ^
+    -DBUILD_TESTS=OFF                               ^
     -DFORCE_LIMITED_API=OFF                         ^
-    .
+    -DPython_EXECUTABLE="%PYTHON%"                  ^
+    -DNO_QT_TOOLS=yes                               ^
+    -B build .
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+cmake --build build --target install
 if errorlevel 1 exit 1
 
+:: write dummy pyside metadata
 mkdir %SP_DIR%\PySide6-%PKG_VERSION%.dist-info
 copy %RECIPE_DIR%\METADATA.pyside6.in %SP_DIR%\PySide6-%PKG_VERSION%.dist-info\METADATA
-copy %RECIPE_DIR%\INSTALLER.in %SP_DIR%\PySide6-%PKG_VERSION%.dist-info\INSTALLER
 echo Version: %PKG_VERSION% >> %SP_DIR%\PySide6-%PKG_VERSION%.dist-info\METADATA
 type %SP_DIR%\PySide6-%PKG_VERSION%.dist-info\METADATA
-
-echo Done: pyside6
-echo Building: pyside-tools
-
-cd %SRC_DIR%\sources\pyside-tools
-mkdir build && cd build
-
-cmake -LAH -G"Ninja"                                ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"          ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"       ^
-    -DCMAKE_BUILD_TYPE=Release                      ^
-    -DPython_EXECUTABLE="%PYTHON%"                  ^
-    ..
-if errorlevel 1 exit 1
-
-cmake --build . --target install
-if errorlevel 1 exit 1
 
 :: Move pyside_tool.py to the right location
 mkdir %SP_DIR%\PySide6\scripts
 type nul > %SP_DIR%\PySide6\scripts\__init__.py
 move %LIBRARY_PREFIX%\bin\pyside_tool.py %SP_DIR%\PySide6\scripts\pyside_tool.py
-
-echo Done: pyside-tools
